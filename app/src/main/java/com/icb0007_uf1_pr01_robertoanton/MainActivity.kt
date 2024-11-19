5e0eaaa58670040708bdaa83736c8a150fb7fe17
Roberto Antón <roberantn@gmail.com>
2024-11-19 23:46:25 +0100

    Instauro primera funcionalidad de la lista de cohetes

--- app/src/main/java/com/icb0007_uf1_pr01_robertoanton/MainActivity.kt
+++ app/src/main/java/com/icb0007_uf1_pr01_robertoanton/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.icb0007_uf1_pr01_robertoanton
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
+// Clase principal de la aplicación para gestionar la navegación entre los fragmentos
 class MainActivity : AppCompatActivity() {
+
+    // Declaración del NavController para controlar la navegación
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // Establece el diseño de la actividad como el contenido de esta actividad
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Configurar la Toolbar personalizada
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar) // Establece la Toolbar como la ActionBar de esta actividad
+
+        // Configuración del NavHostFragment
+        // Este fragmento actúa como un contenedor para manejar la navegación entre otros fragmentos
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController // Obtiene el controlador de navegación
+
+        // Configura la barra de acciones (ActionBar) para trabajar con el NavController
+        setupActionBarWithNavController(navController)
+    }
+
+    // Método para manejar el botón de retroceso en la barra de acciones
+    // Esto asegura que el usuario puede navegar hacia atrás correctamente
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
